90580dba12698d5770792fb910b731f7a497eb8e
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-11-07 15:15:05 +0700

    membuat class navigasi

--- app/src/main/java/com/example/pamprak6/navigasi.kt
+++ app/src/main/java/com/example/pamprak6/navigasi.kt
@@ -1,1 +1,19 @@
 package com.example.pamprak6
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pamprak6.view.FormDataDiri
+import com.example.pamprak6.view.TampilData
+import com.example.pamprak6.view.TampilanAwal
+
+enum class Navigasi {
+    Awal,
+    Formulirku,
+    Detail
+}
